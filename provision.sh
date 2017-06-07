d9c836f7d9543197c6b2a6635ea16073b818d06b
RostislvaZ <zgrostislav@gmail.com>
2017-06-07 17:54:56 +0300

    Inicialisation of project

---
+++ provision.sh
@@ -0,0 +1,10 @@
+apt-get -y update
+
+apt-get -y install nginx
+
+service nginx start
+
+apt-get -y install links
+
+#sh builddocker.sh
+#sudo sh https://gist.github.com/wdullaer/f1af16bd7e970389bad3
